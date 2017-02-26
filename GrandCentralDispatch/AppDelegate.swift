3c18f084e5f87ef3d7899e7687bab8ac5ad20f73
Dan Lindsay <dlindsay72@hotmail.com>
2017-02-25 20:11:19 -0500

    Initial Commit

---
+++ GrandCentralDispatch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GrandCentralDispatch
+//
+//  Created by Dan Lindsay on 2017-02-25.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
